1e48a95dd05b134759e924a79492b24c0ff43c31
nvavinash <avinashmk42@gmail.com>
2023-09-23 23:16:48 +0530

    sorting and rating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SongCard from "./components/SongCard";
+import songs from "./services/songs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state ={
+        songs
+    }
+    sortByTitle = () =>{
+        this.setState({
+        songs:[...this.state.songs.sort((a, b) =>a.title.toUpperCase() < b.title.toUpperCase() ? -1 : 1)]
+    });
+    }
+    sortByRating = () =>{
+        this.setState({
+            songs: [...this.state.songs.sort((a, b) => b.rating - a.rating)]
+        });
+        
+    };
+  render() {
+    return <div id="super-tunes">
+        <h2 id="st-title">SuperTunes - Songs of the Week</h2>
+        <button className="st-btn" onClick={this.sortByTitle}>Sort by Title</button>
+        <button className="st-btn" onClick={this.sortByRating}>Sort by Rating</button>
+        <div className="song-list">
+            {this.state.songs.map((elm) => <SongCard key={elm.id} data={elm}/>)}
+        </div>
+      </div>
+  }
 }
 
 export default App;
